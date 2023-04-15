ddf31d38a0bc1f2b3822c6e3861beeaf60f24105
Chakrit <37141743+chakrits@users.noreply.github.com>
2023-04-15 21:59:19 +0700

    add

---
+++ knowledge/Postman/Date.js
@@ -0,0 +1,6 @@
+// Set Datetime format ##
+
+ // --- Set Current DateTime ---//
+var moment = require('moment');
+var cur_date = moment().format(("DD/MM/YYYY HH:mm:ss"));
+//console.log("Current DateTime = "+ cur_date);
---
+++ knowledge/Postman/json_format.js
@@ -0,0 +1,7 @@
+// --- Set key parameter --- //
+var key = {}
+
+key.{parameter} = {{value}}
+
+// --- convert to json --- //
+var json = JSON.stringify(key)  
