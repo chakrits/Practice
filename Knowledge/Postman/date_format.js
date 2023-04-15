2c16b331b234f3c27981e03cf72f77a236a87ed3
Chakrit <37141743+chakrits@users.noreply.github.com>
2023-04-16 00:00:41 +0700

    Delete Knowledge/Postman directory

--- Knowledge/Postman/date_format.js
+++
@@ -1,5 +0,0 @@
-// --- Set Current DateTime ---//
-var moment = require('moment');
-var cur_date = moment().format(("DD/MM/YYYY HH:mm:ss"));
-//console.log("Current DateTime = "+ cur_date);
-// --- End Set Current DateTime ---//
