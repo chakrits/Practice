4ed03ee497ed92188f91b71e7532952714dcc809
Chakrit <37141743+chakrits@users.noreply.github.com>
2023-07-16 22:02:21 +0700

    add chapter2

---
+++ TextBook/Learn Python Programming/Chapter1/name_and_namespace.py
@@ -0,0 +1,15 @@
+n = 3 # Integer number
+
+address = "221b Baker Street, NW1 6XE, London"  
+
+employee = {
+    'age': 42,
+    'role': 'CTO',
+    'SSN': 'AB1234567'
+}
+
+print(n)
+
+print( address)
+
+print(employee)
